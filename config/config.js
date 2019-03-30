712840be13efd64d734e38bcee20208881de12de
waditya <waditya02@gmail.com>
2019-03-29 21:53:19 -0500

    Updated Database connection settings

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-const config = {
+/* const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
@@ -7,6 +7,19 @@ const config = {
     'mongodb://' + (process.env.IP || 'localhost') + ':' +
     (process.env.MONGO_PORT || '27017') +
     '/TripWithUs2'
+}
+
+export default config */
+
+const config = {
+  env: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 3000,
+  jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+  mongoUri: process.env.MONGODB_URI ||
+    process.env.MONGO_HOST ||
+    'mongodb://' + ('aditya:January1234@ds149404.mlab.com' || 'localhost') + ':' +
+    ('41815' || '27017') +
+    '/tripwithus2'
 }
 
 export default config
