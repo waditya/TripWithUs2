fd6dbc16add89f7876b27f44d2efdfe74d4fd599
waditya <waditya02@gmail.com>
2019-03-29 22:31:15 -0500

    Updated Database Users and Config File again

--- config/config.js
+++ config/config.js
@@ -21,7 +21,7 @@ const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoUri: db.mlab || db.localhost
+  mongoUri: db.mlab
 }
 
 export default config
